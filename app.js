1d6251f0ece032a5d3acadc2b1e8b8e7b31e97fe
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-05-12 08:12:03 +0200

    Ready for deployment.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
-});
+}); 
 
 // Routes
 app.use("/", bookRoutes);
@@ -28,6 +28,6 @@ mongoose
   )
   .then((result) => {
     console.log("CONNECTED");
-    app.listen(8080);
+    app.listen(8081);
   })
   .catch((err) => console.log(err));
